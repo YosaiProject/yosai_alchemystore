b6acfa74731b7fe120ab18ca2786af200224a66a
dowwie <dkgdkg@gmail.com>
2015-11-02 18:14:27 -0500

    added scoped_session

--- models/meta.py
+++ models/meta.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 engine = create_engine("sqlite:///yosai.db", echo=True)
 
-Session = sessionmaker(bind=engine)
+Session = scoped_session(sessionmaker(bind=engine))
 session = Session()
