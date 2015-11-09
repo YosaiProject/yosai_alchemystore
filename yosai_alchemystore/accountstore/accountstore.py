531a4d0b6775b7946e71c0cc0a3e6cebe7675dbb
dowwie <dkgdkg@gmail.com>
2015-11-09 09:17:36 -0500

    migrated updated accountstore from yosai

--- yosai_alchemystore/accountstore/accountstore.py
+++ yosai_alchemystore/accountstore/accountstore.py
@@ -16,31 +16,29 @@ KIND, either express or implied.  See the License for the
 specific language governing permissions and limitations
 under the License.
 """
-
-from accountstore import (
+from ..meta import (
     Session,
 )
 
-from models import (
-    Party,
-    User,
-    Credential,
-    Domain,
-    Action,
-    Resource,
-    Permission,
-    Role,
-    role_permission,
-    role_membership,
+from ..models.models import (
+    Credential as CredentialModel,
+    User as UserModel,
+    Domain as DomainModel,
+    Action as ActionModel,
+    Resource as ResourceModel,
+    Permission as PermissionModel,
+    Role as RoleModel,
+    role_membership as role_membership_table,
+    role_permission as role_permission_table,
+)
+
+from yosai import (
+    Account,
     authz_abcs,
 )
 
 from sqlalchemy import case, func
 import functools
-
-
-class AlchemyAccount:
-    pass
 
 
 def session_context(fn):
@@ -49,19 +47,18 @@ def session_context(fn):
     """
     @functools.wraps(fn)
     def wrap(*args, **kwargs):
-
         session = Session()
-        fn(*args, session=session, **kwargs)
+        result = fn(*args, session=session, **kwargs)
         session.close()
-
+        return result
     return wrap
 
 
-# account_abcs.CredentialsAccountStore, account_abcs.AuthorizationAccountStore
+# account_abcs.CredentialModelsAccountStore, account_abcs.AuthorizationAccountStore
 class AlchemyAccountStore(authz_abcs.PermissionResolverAware,
                           authz_abcs.RoleResolverAware):
     """
-    AlchemyAccountStore provides the realm-facing API to the relational database
+    AccountStore provides the realm-facing API to the relational database
     that is managed through the SQLAlchemy ORM.
 
     step 1:  generate an orm query
@@ -69,10 +66,10 @@ class AlchemyAccountStore(authz_abcs.PermissionResolverAware,
     step 3:  return results
     """
 
-    def __init__(self, permission_resolver=None, role_resolver=None):
+    def __init__(self):
         """
         Since KeyedTuple permissions records have to be converted to an object
-        that yosai can use, it might as well be actual Permission objects.
+        that yosai can use, it might as well be actual PermissionModel objects.
         """
         self._permission_resolver = None  # setter-injected after init
         self._role_resolver = None   # setter-injected after init
@@ -85,43 +82,50 @@ class AlchemyAccountStore(authz_abcs.PermissionResolverAware,
     def permission_resolver(self, permissionresolver):
         self._permission_resolver = permissionresolver
 
+    @property
+    def role_resolver(self):
+        return self._role_resolver
+
+    @role_resolver.setter
+    def role_resolver(self, roleresolver):
+        self._role_resolver = roleresolver
+
     def get_permissions_query(self, session, identifier):
         """
         :type identifier: string
         """
-        thedomain = case([(Domain.name == None, '*')], else_=Domain.name)
-        theaction = case([(Action.name == None, '*')], else_=Action.name)
-        theresource = case([(Resource.name == None, '*')], else_=Resource.name)
+        thedomain = case([(DomainModel.name == None, '*')], else_=DomainModel.name)
+        theaction = case([(ActionModel.name == None, '*')], else_=ActionModel.name)
+        theresource = case([(ResourceModel.name == None, '*')], else_=ResourceModel.name)
 
         action_agg = func.group_concat(theaction.distinct())
         resource_agg = func.group_concat(theresource.distinct())
         perm = (thedomain + ':' + action_agg + ':' + resource_agg).label("perm")
 
         return (session.query(perm).
-                select_from(User).
-                join(role_membership, User.pk_id == role_membership.c.user_id).
-                join(role_permission, role_membership.c.role_id == role_permission.c.role_id).
-                join(Permission, role_permission.c.permission_id == Permission.pk_id).
-                outerjoin(Domain, Permission.domain_id == Domain.pk_id).
-                outerjoin(Action, Permission.action_id == Action.pk_id).
-                outerjoin(Resource, Permission.resource_id == Resource.pk_id).
-                filter(User.identifier == identifier).
-                group_by(Permission.domain_id, Permission.resource_id))
-
+                select_from(UserModel).
+                join(role_membership_table, UserModel.pk_id == role_membership_table.c.user_id).
+                join(role_permission_table, role_membership_table.c.role_id == role_permission_table.c.role_id).
+                join(PermissionModel, role_permission_table.c.permission_id == PermissionModel.pk_id).
+                outerjoin(DomainModel, PermissionModel.domain_id == DomainModel.pk_id).
+                outerjoin(ActionModel, PermissionModel.action_id == ActionModel.pk_id).
+                outerjoin(ResourceModel, PermissionModel.resource_id == ResourceModel.pk_id).
+                filter(UserModel.identifier == identifier).
+                group_by(PermissionModel.domain_id, PermissionModel.resource_id))
 
     def get_roles_query(self, session, identifier):
         """
         :type identifier: string
         """
-        return (session.query(Role).
-                join(role_membership, Role.pk_id == role_membership.role_id).
-                join(User, role_membership.user_id == User.pk_id).
-                filter(User.identifier == identifier))
+        return (session.query(RoleModel).
+                join(role_membership_table, RoleModel.pk_id == role_membership_table.c.role_id).
+                join(UserModel, role_membership_table.c.user_id == UserModel.pk_id).
+                filter(UserModel.identifier == identifier))
 
     def get_credential_query(self, session, identifier):
-        return (session.query(Credential.password).
-                join(User, Credential.user_id == User.pk_id).
-                filter(User.identifier == identifier))
+        return (session.query(CredentialModel.credential).
+                join(UserModel, CredentialModel.user_id == UserModel.pk_id).
+                filter(UserModel.identifier == identifier))
 
     @session_context
     def get_account(self, authc_token, session=None):
@@ -135,19 +139,19 @@ class AlchemyAccountStore(authz_abcs.PermissionResolverAware,
         identifier = authc_token.identifier
 
         credential = (self.get_credential_query(session, identifier).
-                      scalar().credential)
+                      scalar())
 
         perms = self.get_permissions_query(session, identifier).all()
         permissions = {self.permission_resolver(permission=p.perm)
                        for p in perms}
 
-        roles = {self.role_resolver(title=r.title)
+        roles = {self.role_resolver(r.title)
                  for r in self.get_roles_query(session, identifier).all()}
 
-        account = AlchemyAccount(account_id=identifier,
-                                 credentials=credential,
-                                 permissions=permissions,
-                                 roles=roles)
+        account = Account(account_id=identifier,
+                          credentials=credential,
+                          permissions=permissions,
+                          roles=roles)
 
         return account
 
@@ -160,8 +164,8 @@ class AlchemyAccountStore(authz_abcs.PermissionResolverAware,
 
         credential = self.get_credential_query(session, identifier).scalar()
 
-        account = AlchemyAccount(account_id=identifier,
-                                 credentials=credential)
+        account = Account(account_id=identifier,
+                          credentials=credential)
 
         return account
 
@@ -170,13 +174,17 @@ class AlchemyAccountStore(authz_abcs.PermissionResolverAware,
         """
         :returns: Account
         """
-        identifier = authc_token.identifier
 
-        permissions = self.get_permissions_query(session, identifier).all()
-        roles = self.get_roles_query(session, identifier).all()
+        perms = self.get_permissions_query(session, identifier).all()
 
-        account = AlchemyAccount(account_id=identifier,
-                                 permissions=permissions,
-                                 roles=roles)
+        permissions = {self.permission_resolver(permission=p.perm)
+                       for p in perms}
+
+        roles = {self.role_resolver(r.title)
+                 for r in self.get_roles_query(session, identifier).all()}
+
+        account = Account(account_id=identifier,
+                          permissions=permissions,
+                          roles=roles)
 
         return account
